37e63996ff33fc001e216c134009308841727fc8
thiyagu <thiyagu@programmer.net>
2020-09-27 21:18:20 +0530

    initial

---
+++ lib/Costumers/Costumers.php
@@ -0,0 +1,52 @@
+<?php
+class Costumers
+{
+    /**
+     *
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     *
+     */
+    public function __destruct()
+    {
+    }
+    
+    /**
+     * Set friendly columns\' names to order tables\' entries
+     */
+    public function setOrderingValues()
+    {
+        $ordering = [
+            'id' => 'ID',
+            'f_name' => 'First Name',
+            'l_name' => 'Last Name',
+            'gender' => 'Gender',
+            'phone' => 'Phone',
+            'created_at' => 'Created at',
+            'updated_at' => 'Updated at'
+        ];
+
+        return $ordering;
+    }
+
+
+
+    public function setOrderingValuesScore()
+    {
+        $ordering = [
+            'id' => 'ID',
+            'f_name' => 'First Name',
+            'l_name' => 'Last Name',
+             'current_score' => 'Current Score',
+              'maximum_score' => '  Maximum Score'
+          
+        ];
+
+        return $ordering;
+    }
+}
+?>
